5081edd22d6aa351db44dce7f0e150314606e97a
sainath tingilkar <tingilkarsainath0@gmail.com>
2026-08-01 11:36:27 +0530

    Build home page hero section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home.jsx"
 
 function App() {
   return (
-    <div>
+    <div className='w-screen min-h-screen bg-richblack-900 flex flex-col font-inter'>
       <Routes>
         <Route path = "/" element={<Home/>} />
       </Routes>
