339c9e24d77e49810347843c4859759f96750e6a
sainath tingilkar <tingilkarsainath0@gmail.com>
2026-08-07 15:48:29 +0530

    Setup Redux Toolkit store and slices

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Routes,Route} from "react-router-dom"
 import Home from "./pages/Home.jsx"
+import Navbar from './components/common/Navbar.jsx'
 
 function App() {
   return (
     <div className='w-screen min-h-screen bg-richblack-900 flex flex-col font-inter'>
+      <Navbar/>
       <Routes>
         <Route path = "/" element={<Home/>} />
       </Routes>
---
+++ client/src/reducer/index.js
@@ -0,0 +1,12 @@
+import {combineReducers} from "@reduxjs/toolkit"
+import authReducer from "../slices/authSlice.js"
+import profileReducer from "../slices/profileSlice.js"
+import cartReducer from "../slices/cartSlice.js"
+
+const rootReducer = combineReducers({
+    auth:authReducer,
+    profile:profileReducer,
+    cart:cartReducer,
+})
+
+export default rootReducer;
