d4ae166b8ee3fe8f434f6d7927c8a9ffbc15ddf2
sainath tingilkar <tingilkarsainath0@gmail.com>
2026-08-12 17:34:28 +0530

    Implement Dashboard page UI

--- client/src/components/common/ReviewSlider.jsx
+++ client/src/components/common/ReviewSlider.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ReviewSlider = () => {
   return (
     <div>
-      
+      Review slider
     </div>
   )
 }
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,5 +1,5 @@
 import signupImg from "../assets/images/signup.jpg"
-import Template from "../components/core/Auth/Template"
+import Template from "../components/core/Auth/Template.jsx"
 
 function Signup() {
   return (
